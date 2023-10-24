325ede0e6523a8f20e32aee7ba77014dbcad6074
Olegezh <113126686+Olegezh@users.noreply.github.com>
2023-10-24 10:39:40 +0300

    Add files via upload

---
+++ HomeWork4/services/bancRepo.py
@@ -0,0 +1,12 @@
+class BancRepo:
+    """МОДЕЛИРУМ ОТВЕТ БАНКА ПОСЛЕ ПРОВЕРКИ ДОСТАТОЧНОСТИ СРЕДСТВ"""
+
+    def __init__(self):
+        self.banc_data={}
+        self.banc_data={1234567898765432: 10000}
+    def request(self, bill, cardNumber):
+        if bill < self.banc_data[cardNumber]:
+            self.banc_data[cardNumber] = self.banc_data[cardNumber]-bill
+            return True
+        else:
+            return False
